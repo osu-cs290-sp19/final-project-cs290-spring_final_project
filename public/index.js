65ad2a272fe2309d70e9bb2e610e903016b88af4
dongrogers <dongrogers>
2019-06-12 03:43:27 -0700

    Sell pet modal added

--- public/index.js
+++ public/index.js
@@ -1,3 +1,25 @@
+// === MODAL INTERACTION === //
+var openModalButton = document.querySelector('.sell-pet-button');
+var closeModalButton = document.querySelector('.modal-close-button');
+var modalBackdrop = document.querySelector('#modal-backdrop');
+var sellPetModal = document.querySelector('#sell-pet-modal');
+var sellPetButton = document.querySelector('.sell-pet-submit');
+closeModalButton.addEventListener('click', function(){
+	modalBackdrop.classList.add('hidden');
+	sellPetModal.classList.add('hidden');
+})
+
+openModalButton.addEventListener('click', function(){
+	modalBackdrop.classList.remove('hidden');
+	sellPetModal.classList.remove('hidden');
+})
+
+sellPetButton.addEventListener('click', function(){
+	//Still needs to check if forms are filled before closing
+	modalBackdrop.classList.add('hidden');
+	sellPetModal.classList.add('hidden');
+})
+
 // === SORTING/NAVIGATION === //
 
 var navItems = document.querySelectorAll('.navitem');
