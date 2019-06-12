be6449547e68572074739276ee897a4151413068
dongrogers <dongrogers>
2019-06-12 05:49:47 -0700

    Added some views, worked on async/db stuff

--- public/index.js
+++ public/index.js
@@ -4,6 +4,17 @@ var closeModalButton = document.querySelector('.modal-close-button');
 var modalBackdrop = document.querySelector('#modal-backdrop');
 var sellPetModal = document.querySelector('#sell-pet-modal');
 var sellPetButton = document.querySelector('.sell-pet-submit');
+
+var sellPetName = document.querySelector('.sell-pet-name');
+var sellPetColor = document.querySelector('.sell-pet-color');
+var sellPetSpecies = document.querySelector('.sell-pet-species');
+var sellPetPicture = document.querySelector('.sell-pet-picture');
+
+function hideModal(){
+	modalBackdrop.classList.add('hidden');
+	sellPetModal.classList.add('hidden');
+}
+
 closeModalButton.addEventListener('click', function(){
 	modalBackdrop.classList.add('hidden');
 	sellPetModal.classList.add('hidden');
@@ -20,8 +31,65 @@ sellPetButton.addEventListener('click', function(){
 	sellPetModal.classList.add('hidden');
 })
 
-// === SORTING/NAVIGATION === //
+// === LOAD PETS === //
+function loadProduct(petName, url){
+	var petObject = {
+		petname: petName,
+		url: url,
+	};
 
+	var productHTML = Handlebars.templates.product(petObject);
+	return productHTML;
+}
+
+// === SELL PET === //
+function handleSellPetClick(){
+	var sellPetName = document.querySelector('.sell-pet-name').value.trim();
+	var sellPetColor = document.querySelector('.sell-pet-color').value;
+	var sellPetSpecies = document.querySelector('.sell-pet-species').value;
+	var sellPetPicture = document.querySelector('.sell-pet-picture').value.trim();
+
+	
+	/*
+	var request = XMLHttpRequest();
+	var requestURL = '/';
+	request.open('POST', requestURL);
+
+	var petObj = {
+					petName: sellPetName,
+					petColor: sellPetColor,
+					petSpecies: sellPetSpecies,
+					petURL: sellPetPicture,
+	};
+	
+	var requestBody = JSON.stringify(petObj);
+	request.setRequestHeader(
+		'Content-Type','application/json'
+	);
+	
+	request.addEventListener('load', function(event){		
+		if(event.target.status != 200){
+			var msg = event.target.response
+			alert("Error storing pet data: " + msg);
+		}
+	*/
+
+		if(!sellPetName || !sellPetPicture){
+			alert("Insert Info!");
+		}
+		else{
+			alert("Good");
+			var productHTML = loadProduct(sellPetName, sellPetPicture);
+			var productContainer = document.querySelector('.photo-card-container');
+			productContainer.insertAdjacentHTML('beforeend', productHTML);
+			hideModal();
+		}
+	//});
+	
+	// request.send(requestBody);
+}
+
+// === SORTING/NAVIGATION === //
 var navItems = document.querySelectorAll('.navitem');
 //For each nav item, add event listener
 navItems.forEach(function(event){
@@ -34,4 +102,12 @@ navItems.forEach(function(event){
         item.classList.remove('active');
     })
   });
+});
+
+// === LISTENERS === //
+window.addEventListener('DOMContentLoaded', function () {
+
+  var modalAcceptButton = document.querySelector('.sell-pet-submit');
+  modalAcceptButton.addEventListener('click', handleSellPetClick); 
+
 });
