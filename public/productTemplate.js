a619843d7e490b95952f2cf8e18b47d0e61985f7
dongrogers <dongrogers>
2019-06-12 20:58:43 -0700

    Fixed push

--- public/productTemplate.js
+++ public/productTemplate.js
@@ -1,21 +1,17 @@
 (function() {
   var template = Handlebars.template, templates = Handlebars.templates = Handlebars.templates || {};
-templates['product'] = template({"1":function(container,depth0,helpers,partials,data) {
-    var helper;
-
-  return "		<h2 class=\"product-color\"> "
-    + container.escapeExpression(((helper = (helper = helpers.color || (depth0 != null ? depth0.color : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0 != null ? depth0 : (container.nullContext || {}),{"name":"color","hash":{},"data":data}) : helper)))
-    + " </h2>\n";
-},"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
-    var stack1, helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
+templates['product'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
+    var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
   return "<div class=\"product\">\n	<img class=\"product-image\" src=\""
     + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
     + "\">\n	<div class=\"product-meta\">\n		<h2 class=\"product-text\"> "
     + alias4(((helper = (helper = helpers.petname || (depth0 != null ? depth0.petname : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"petname","hash":{},"data":data}) : helper)))
-    + " </h2>\n\n"
-    + ((stack1 = helpers["if"].call(alias1,(depth0 != null ? depth0.hideInfo : depth0),{"name":"if","hash":{},"fn":container.program(1, data, 0),"inverse":container.noop,"data":data})) != null ? stack1 : "")
-    + "\n		<p class=\"product-price\"> "
+    + " </h2>\n\n		<h2 class=\"product-species\"> "
+    + alias4(((helper = (helper = helpers.species || (depth0 != null ? depth0.species : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"species","hash":{},"data":data}) : helper)))
+    + " </h2>\n		<h2 class=\"product-color\"> "
+    + alias4(((helper = (helper = helpers.color || (depth0 != null ? depth0.color : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"color","hash":{},"data":data}) : helper)))
+    + " </h2>\n\n\n		<p class=\"product-price\"> "
     + alias4(((helper = (helper = helpers.petprice || (depth0 != null ? depth0.petprice : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"petprice","hash":{},"data":data}) : helper)))
     + " </p>\n	</div>\n</div>\n";
 },"useData":true});
--- server.js
+++ server.js
@@ -6,11 +6,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = process.env.PORT || 9991;
 
-<<<<<<< HEAD
 var petData = require('./petData');
-=======
-var animalData = require('./animalData');
->>>>>>> 79f58f7da75e37ef0ac1dab76b94e95fa8886eb2
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -18,45 +14,17 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 
-var thrown = {
-	products: animalData
-}
-
 
 //Page shows all pets for sale
 app.get('/', function(req, res, next){
-<<<<<<< HEAD
-	var dogs = petData['dogs'];
-	var cats = petData['cats'];
-=======
-
-	res.status(200).render('productPage', thrown);
-});
-
-app.get('/pets/:animals',function(req,res,next){
-	res.status(200).render('productPage', thrown);
-
-
-
-});
->>>>>>> 79f58f7da75e37ef0ac1dab76b94e95fa8886eb2
-
 	res.render('productPage', petData);
 });
 
 
-<<<<<<< HEAD
 app.get('/:species', function (req, res, next){
 	var pet = req.params.species;
 	if (petData[pet]){
 		res.status(200).render('productPage', petData[pet]);
-=======
-
-/*app.get('/pets/:animals', function (req, res, next){
-	var animal = req.params.animals.toLowerCase();
-	if (animalData[person]){
-		res.status(200).render('animalPage', animalData[animal]);
->>>>>>> 79f58f7da75e37ef0ac1dab76b94e95fa8886eb2
 	}
 	else{
 		next();
