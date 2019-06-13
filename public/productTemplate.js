b54f15838de2f6e9f5be5639ee0ca3687de31d93
unknown <liji2@oregonstate.edu>
2019-06-13 02:44:29 -0700

    Link Changes

--- public/productTemplate.js
+++ public/productTemplate.js
@@ -3,12 +3,14 @@
 templates['product'] = template({"compiler":[7,">= 4.0.0"],"main":function(container,depth0,helpers,partials,data) {
     var helper, alias1=depth0 != null ? depth0 : (container.nullContext || {}), alias2=helpers.helperMissing, alias3="function", alias4=container.escapeExpression;
 
-  return "<div class=\"product hidden\">\n	<img class=\"product-image\" src=\""
-    + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
-    + "\">\n	<div class=\"product-meta\">\n		<h2 class=\"product-text\"> "
+  return "<a href=\"/"
     + alias4(((helper = (helper = helpers.petname || (depth0 != null ? depth0.petname : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"petname","hash":{},"data":data}) : helper)))
-    + " </h2>\n		<p class=\"product-price\"> "
+    + "\" class=\"product\">\r\n	<img class=\"product-image\" src=\""
+    + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
+    + "\">\r\n	<div class=\"product-meta\">\r\n		<h2 class=\"product-text\"> "
+    + alias4(((helper = (helper = helpers.petname || (depth0 != null ? depth0.petname : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"petname","hash":{},"data":data}) : helper)))
+    + " </h2>\r\n		<p class=\"product-price\"> "
     + alias4(((helper = (helper = helpers.petprice || (depth0 != null ? depth0.petprice : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"petprice","hash":{},"data":data}) : helper)))
-    + " </p>\n	</div>\n</div>\n";
+    + " </p>\r\n	</div>\r\n</a>\r\n";
 },"useData":true});
 })();
