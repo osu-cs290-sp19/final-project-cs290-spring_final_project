33c34c7df740ec18572825e249fef289faad2a39
David Chan <chanda@flip1.engr.oregonstate.edu>
2019-06-13 00:09:32 -0700

    Fixed button display

--- public/productTemplate.js
+++ public/productTemplate.js
@@ -7,11 +7,7 @@ templates['product'] = template({"compiler":[7,">= 4.0.0"],"main":function(conta
     + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
     + "\">\n	<div class=\"product-meta\">\n		<h2 class=\"product-text\"> "
     + alias4(((helper = (helper = helpers.petname || (depth0 != null ? depth0.petname : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"petname","hash":{},"data":data}) : helper)))
-    + " </h2>\n\n		<h2 class=\"product-species\"> "
-    + alias4(((helper = (helper = helpers.species || (depth0 != null ? depth0.species : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"species","hash":{},"data":data}) : helper)))
-    + " </h2>\n		<h2 class=\"product-color\"> "
-    + alias4(((helper = (helper = helpers.petcolor || (depth0 != null ? depth0.petcolor : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"petcolor","hash":{},"data":data}) : helper)))
-    + " </h2>\n\n		<p class=\"product-price\"> "
+    + " </h2>\n		<p class=\"product-price\"> "
     + alias4(((helper = (helper = helpers.petprice || (depth0 != null ? depth0.petprice : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"petprice","hash":{},"data":data}) : helper)))
     + " </p>\n	</div>\n</div>\n";
 },"useData":true});
