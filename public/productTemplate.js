d04e585ea576279481ca2fd55da330589d9462b0
dongrogers <dongrogers>
2019-06-13 20:52:59 -0700

    Start mongo

--- public/productTemplate.js
+++ public/productTemplate.js
@@ -5,14 +5,14 @@ templates['product'] = template({"compiler":[7,">= 4.0.0"],"main":function(conta
 
   return "<a href=\"/"
     + alias4(((helper = (helper = helpers.petname || (depth0 != null ? depth0.petname : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"petname","hash":{},"data":data}) : helper)))
-    + "\" class=\"product\">\r\n	<img class=\"product-image\" src=\""
+    + "\" class=\"product\">\n	<img class=\"product-image\" src=\""
     + alias4(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"url","hash":{},"data":data}) : helper)))
-    + "\">\r\n	<div class=\"product-meta\">\r\n		<h2 class=\"product-text\"> "
+    + "\">\n	<div class=\"product-meta\">\n		<h2 class=\"product-text\"> "
     + alias4(((helper = (helper = helpers.petname || (depth0 != null ? depth0.petname : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"petname","hash":{},"data":data}) : helper)))
     + " ("
     + alias4(((helper = (helper = helpers.petspecies || (depth0 != null ? depth0.petspecies : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"petspecies","hash":{},"data":data}) : helper)))
-    + ")</h2>\r\n		<p class=\"product-price\"> "
+    + ")</h2>\n		<p class=\"product-price\"> "
     + alias4(((helper = (helper = helpers.petprice || (depth0 != null ? depth0.petprice : depth0)) != null ? helper : alias2),(typeof helper === alias3 ? helper.call(alias1,{"name":"petprice","hash":{},"data":data}) : helper)))
-    + " </p>\r\n	</div>\r\n</a>\r\n\r\n";
+    + " </p>\n	</div>\n</a>\n\n";
 },"useData":true});
 })();
--- server.js
+++ server.js
@@ -4,12 +4,22 @@ var express = require('express');
 var exphbs= require('express-handlebars');
 var bodyParser = require('body-parser');
 var content = fs.readFileSync("petData.json");
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 var port = process.env.PORT || 9991;
 
 var petData = require('./petData');
 var count = 0;
+
+var mongoHost = process.env.MONGO_HOST; //classmongo.engr.oregonstate.edu
+var mongoPort = process.env.MONGO_PORT || 27017;
+var mongoUser = process.env.MONGO_USER; //cs290_dongrog
+var mongoPassword = process.env.MONGO_PASSWORD; //rogers
+var mongoDBName = process.env.MONGO_DB_NAME; //cs290_dongrog
+
+var mongoUrl = `mongodb://${mongoUser}:${mongoPassword}@${mongoHost}:${mongoPort}/${mongoDBName}`;
+var db = null;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -118,10 +128,12 @@ app.get('*', function(req,res){
 	res.status(404).render('errorPage');
 });
 
-
-app.listen(port, function (err) {
+MongoClient.connect(mongoUrl, function (err, client) {
   if (err) {
     throw err;
   }
-  console.log("== Server listening on port", port);
+  db = client.db(mongoDBName);
+  app.listen(port, function () {
+    console.log("== Server listening on port", port);
+  });
 });
